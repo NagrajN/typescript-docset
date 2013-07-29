6875c4fdd9bb6273a947166305df50cd56597733
Nagraj Naidu <nagrajn@live.com>
2013-07-29 07:24:02 -0400

    refactored parser

--- parser/parser.js
+++ parser/parser.js
@@ -1,24 +1,24 @@
 
 var fs = require('fs');
 var Sequelize = require('sequelize');
-var filePathDefault = '../TypeScript.docset/Contents/Resources/Documents/index.html';
-var sqlite3Default = '../TypeScript.docset/Contents/Resources/docSet.dsidx';
-var indexArr = [];
+var resource = '../TypeScript.docset/Contents/Resources/Documents/index.html';
+var db = '../TypeScript.docset/Contents/Resources/docSet.dsidx';
+var indexes = [];
 
-filePathDefault = (!!process.argv[2]) ? process.argv[2] : filePathDefault;
-sqlite3Default = (!!process.argv[3]) ? process.argv[3] : sqlite3Default;
+resource = (!!process.argv[2]) ? process.argv[2] : resource;
+db = (!!process.argv[3]) ? process.argv[3] : db;
 
-var fileData = fs.readFileSync(filePathDefault, {encoding : 'ascii'});
+var fileData = fs.readFileSync(resource, {encoding : 'utf8'});
 fileData = fileData.replace(/[\r\n\t]*/g,'');
 
 function extract(regEx, docType) {
-	var name, type, path;
-	while(result = regEx.exec(fileData)) {
-		path = result[0].match(/<a href="(#[\s\S]*?)">/)[1];
-		tmp = result[0].match(/<span style='mso-no-proof:yes'>([\s\S]*?)<\/span>/g);
+	var name, type, path, tmp;
+	while(rslt = regEx.exec(fileData)) {
+		path = rslt[0].match(/<a href="(#[\s\S]*?)">/)[1];
+		tmp = rslt[0].match(/<span style='mso-no-proof:yes'>([\s\S]*?)<\/span>/g);
 		if (tmp) {
-			name = result[0].match(/<span style='mso-no-proof:yes'>([\s\S]*?)<\/span>/g)[1].match(/[\s\S]*>([\s\S]*?)<\/[\s\S]*/)[1];
-			indexArr.push({"name" : name,"type": docType,"path": "index.html" + path});
+			name = rslt[0].match(/<span style='mso-no-proof:yes'>([\s\S]*?)<\/span>/g)[1].match(/[\s\S]*>([\s\S]*?)<\/[\s\S]*/)[1];
+			indexes.push({"name":name,"type":docType,"path":"index.html" + path});
 		}
 	}
 }
@@ -28,11 +28,11 @@ extract(/<p class=MsoToc3[\s\S]*?>([\s\S]*?)<\/p>/g, 'Tag');
 
 var sequelize = new Sequelize('database', 'username', 'password', {
   dialect: 'sqlite',
-  storage: sqlite3Default
+  storage: db
 });
 
 sequelize.query('DELETE FROM searchIndex').success(function() {
-	indexArr.forEach(function(item){
+	indexes.forEach(function(item){
 		sequelize.query("INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES ('" + item.name + "', '" + item.type + "', '" + item.path + "');");
 	})
 })
