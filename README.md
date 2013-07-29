813c7a59542e7bb9fd01234f6b3bf70aae0079b5
Nagraj Naidu <nagrajn@live.com>
2013-07-28 21:46:28 -0400

    updated parser for toc2 and toc3

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
 # TypeScript Doc Set
 
-This is TypeScript DocSet with mimimal indexing.
+This is TypeScript DocSet with some indexing. The source of this Doc Set is the Microsoft TypeScript Specification.
 
 I built it so I can use the docset with Dash on my Mac
 
 The docset is in TypeScript.docset directory. You can add this in Dash or any other tooling
 
-The parser directory contains a simple JS parser that runs on Node. To run the parser use npm install to install required dependencies.
+The parser directory contains a simple JS parser that runs on Node. To run the parser use npm install for required dependencies.
+
+There are two "sections" to the Doc Set
+(1) Guides: The top level breakdown as in the Specification Document
+(2) Tags: An index of almost everything that is taggable in the document.
 
 ## License
 Copyright (c) 2013 "nagrajn" Nagraj Naidu & contributors.
--- parser/parser.js
+++ parser/parser.js
@@ -1,10 +1,10 @@
 
 var fs = require('fs');
 var Sequelize = require('sequelize');
-
 var filePathDefault = "../TypeScript.docset/Contents/Resources/Documents/index.html";
 var sqlite3Default = "../TypeScript.docset/Contents/Resources/docSet.dsidx";
 var name, type, path; 
+var indexArr = [];
 
 if (!!process.argv[2]) {
 	filePathDefault = process.argv[2];
@@ -17,16 +17,46 @@ if (!!process.argv[3]) {
 var fileData = fs.readFileSync(filePathDefault, {encoding : 'ascii'});
 fileData = fileData.replace(/[\r\n\t]*/g,"");
 
-var indexArr = [];
+var guideRegex = /<p class=MsoToc1>([\s\S]*?)<\/p>/g;
 
-var regex = /<p class=MsoToc1>([\s\S]*?)<\/p>/g;
-
-while(result = regex.exec(fileData)) {
+while(result = guideRegex.exec(fileData)) {
 	type = "Guide";
 	name = result[0].match(/<span style='mso-no-proof:yes'>[\s\S]*?<\/span>/g)[1].match(/[\s\S]*>([\s\S]*?)<\/[\s\S]*/)[1];
 	path = result[0].match(/<a href="(#[\s\S]*?)">/)[1];
-	indexArr.push({"name" : name,"type": type,"path": "index.html" + path});
+	indexArr.push({"name" : name,"type": type,"path": "index.html" + path });
 }
+
+var toc2Regex = /<p class=MsoToc2[\s\S]*?>([\s\S]*?)<\/p>/g;
+
+while(result = toc2Regex.exec(fileData)) {
+	console.log(result[0]);
+	type = "Tag";
+	path = result[0].match(/<a href="(#[\s\S]*?)">/)[1];
+
+	tmp = result[0].match(/<span style='mso-no-proof:yes'>([\s\S]*?)<\/span>/g);
+	if (tmp) {
+		name = result[0].match(/<span style='mso-no-proof:yes'>([\s\S]*?)<\/span>/g)[1].match(/[\s\S]*>([\s\S]*?)<\/[\s\S]*/)[1];
+		indexArr.push({"name" : name,"type": type,"path": "index.html" + path});
+	}
+
+}	
+
+//refactor
+var toc3Regex = /<p class=MsoToc3[\s\S]*?>([\s\S]*?)<\/p>/g;
+
+while(result = toc3Regex.exec(fileData)) {
+	console.log(result[0]);
+	type = "Tag";
+	path = result[0].match(/<a href="(#[\s\S]*?)">/)[1];
+
+	tmp = result[0].match(/<span style='mso-no-proof:yes'>([\s\S]*?)<\/span>/g);
+	if (tmp) {
+		name = result[0].match(/<span style='mso-no-proof:yes'>([\s\S]*?)<\/span>/g)[1].match(/[\s\S]*>([\s\S]*?)<\/[\s\S]*/)[1];
+		indexArr.push({"name" : name,"type": type,"path": "index.html" + path});
+	}
+
+}	
+
 
 var sequelize = new Sequelize('database', 'username', 'password', {
   dialect: 'sqlite',
@@ -38,7 +68,6 @@ sequelize.query("DELETE FROM searchIndex").success(function() {
 	indexArr.forEach(function(item){
 
 		sequelize.query("INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES ('" + item.name + "', '" + item.type + "', '" + item.path + "');").success(function() {
-			console.log("inserted");
 		})
 
 	})
